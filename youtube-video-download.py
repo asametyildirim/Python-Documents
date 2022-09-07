873d77f406a6ca4e98845df877223e635472dba3
AbdulSametYildirim <sametyldrym2@gmail.com>
2022-09-07 16:24:11 +0300

    Update youtube video download

--- youtube-video-download.py
+++ youtube-video-download.py
@@ -1,5 +1,21 @@
 from pytube import YouTube
 
+""" 
 get_link = YouTube("https://www.youtube.com/watch?v=wLlovxa3VJ0&ab_channel=CelestialDraconis")
 show_stream = get_link.streams.filter(progressive="True").first()
 show_stream.download()
+"""
+
+def download_video:
+	url = Youtube(input("Please enter the youtube url"))
+	download_link = url.streams.filter(progressive="True").first()
+	download_link.download()
+
+	print("-"*25)
+	print("Video title: ",url.title)
+	print("Video author: ",url.author)
+	print("Video viewer count: ",url.views)
+	print("Video lenght: ",url.lenght,"second")
+	print("-"*25)
+
+download_video()
